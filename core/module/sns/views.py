bd0f954a2f1b3ac15f289c8d0c36d05274c7a20d
root <root@yuz.yuz.com>
2014-05-22 17:30:33 +0800

    init first template page and create service

--- core/module/sns/views.py
+++ core/module/sns/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint,request
+from flask import Blueprint,request,render_template
+from core.module.sns.service import *
 
 sns_blue = Blueprint('sns_blue',__name__,url_prefix='/auth')
 
-@sns_blue.route('/index')
+@sns_blue.route('/index',methods=['GET','POST'])
 def index():
-	print 'hello'
-	return 'what the fuck'
+	return  render_template('index.html')
