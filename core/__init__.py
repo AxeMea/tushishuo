9dcb08b999dde681658416724ba9decdcf54664d
Administrator <yuzheng@ubuntu.ubuntu-domain>
2014-06-02 22:33:51 +0800

    update

--- core/__init__.py
+++ core/__init__.py
@@ -7,8 +7,8 @@ from core.config.config import *
 
 app = Flask(__name__)
 app.secret_key = 'why would I tell you my secret key?'
-
 app.config.from_object('config')
+
 
 db = SQLAlchemy(app)
 
@@ -20,7 +20,8 @@ if(INIT_DATABASE):
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "/index.html"
-  
+ 
+
 #logging.basicConfig(filename=dirname(__file__)+"/blueprint.log", level=logging.INFO, filemode='a', format='%(asctime)s - %(levelname)s: %(message)s')
 
 from core.module.sns.views import  sns_blue 
--- core/module/sns/views.py
+++ core/module/sns/views.py
@@ -1,10 +1,43 @@
-from flask import Blueprint,request,render_template
+from flask import Blueprint,request,render_template,g,session
 from core.module.sns.service import *
 from core.module.sns.forms import *
+from core.share.model.models import *
+#from flask_login import LoginManager
+#from core import login_manager,app
 
 sns_blue = Blueprint('sns_blue',__name__,url_prefix='/auth')
 
-@sns_blue.route('/index',methods=['GET','POST'])
+@sns_blue.route('/login',methods=['GET','POST'])
+def login():
+	if g.user is not None and g.user.is_authenticated():
+		return redirect('/auth/index')
+
+	form = LoginForm(request.form)
+
+	if form.validate():
+		user = User.query.filter_by(email = form.email.data).first()
+
+		if user is None:
+			return render_template('/sns/index.html',form = form)
+		else:
+			return render_template('/sns/index.html',current_user = {'username':user.username})
+
+	return  render_template('/sns/index.html',form=form)
+
+
+@sns_blue.route('/index',methods=['POST','GET'])
 def index():
 	form = LoginForm()
-	return  render_template('/sns/index.html',form=form)
+	return render_template('/sns/index.html',form = form)
+
+@sns_blue.route('/logout',methods=['POST','GET'])
+def logout():
+	#logout_user()
+	return redirect(url_for('auth/index')
+
+#@login_manager.user_loader
+#def load_user(id):
+#	return User.query.get(int(id))
+#@app.before_request
+#def before_request():
+#    g.user = current_usere
